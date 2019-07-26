41400270b4212604a1adf70360d4774ed2c9fbfb
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-26 13:56:35 -0500

    added .gitignore

---
+++ ConsoleAppCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleAppCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- This is an arithmetic claculator ---");
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("add: Addition\n");
+            sb1.Append("sub:Subtraction\n");
+            sb1.Append("mult:Multiplication\n");
+            sb1.Append("div:Division");
+            Console.WriteLine("Type your commands here:\n" + sb1.ToString());
+            Console.Write(">:");
+            string choice = Console.ReadLine().ToString().ToLower();
+        }
+    }
+}
