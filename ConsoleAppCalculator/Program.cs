3423937b46dcd280f3d5ad26749a83c5e44fb659
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-26 14:17:56 -0500

    added try-catch and while loop

--- ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/Program.cs
@@ -5,19 +5,19 @@ namespace ConsoleAppCalculator
 {
     class Program
     {
-        static void add(double n1, double n2)
+        static void add(int n1, int n2)
         {
             Console.WriteLine("The Result is:{0}\n", n1 + n2);
         }
-        static void sub(double n1, double n2)
+        static void sub(int n1, int n2)
         {
-            Console.WriteLine("The Result is:{0)\n", n1 - n2);
+            Console.WriteLine("The Result is:{0}\n", n1 - n2);
         }
-        static void mult(double n1, double n2)
+        static void mult(int n1, int n2)
         {
             Console.WriteLine("The Result is:{0}\n", n1 * n2);
         }
-        static void div(double n1, double n2)
+        static void div(int n1, int n2)
         {
             if (n2 == 0)
             {
@@ -31,39 +31,53 @@ namespace ConsoleAppCalculator
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("--- This is an arithmetic claculator ---");
-            StringBuilder sb1 = new StringBuilder();
-            sb1.Append("add: Addition\n");
-            sb1.Append("sub:Subtraction\n");
-            sb1.Append("mult:Multiplication\n");
-            sb1.Append("div:Division");
-            Console.WriteLine("Type your commands here:\n" + sb1.ToString());
-            Console.Write(">:");
-            string choice = Console.ReadLine().ToString().ToLower();
-            Console.WriteLine("Enter two numbers:");
-            string n1 = Console.ReadLine();
-            string n2 = Console.ReadLine();
-            int num1 = int.Parse(n1);
-            int num2 = int.Parse(n2);
-            switch (choice)
+            string choice = "";
+            do
             {
-                case "add":
-                    add(num1, num2);
-                    break;
-                case "sub":
-                    sub(num1, num2);
-                    break;
-                case "mult":
-                    mult(num1, num2);
-                    break;
-                case "div":
-                    div(num1, num2);
-                    break;
-                default:
-                    //default command error message 
-                    Console.WriteLine("Bad Command");
-                    break;
-            }
+                try
+                {
+                    Console.WriteLine("--- This is an arithmetic claculator ---");
+                    Console.WriteLine("Enter two numbers:");
+                    string n1 = Console.ReadLine();
+                    string n2 = Console.ReadLine();
+                    int num1 = int.Parse(n1);
+                    int num2 = int.Parse(n2);
+                    StringBuilder sb1 = new StringBuilder();
+                    sb1.Append("add: Addition\n");
+                    sb1.Append("sub:Subtraction\n");
+                    sb1.Append("mult:Multiplication\n");
+                    sb1.Append("div:Division\n");
+                    sb1.Append("exit:Exit");
+                    Console.WriteLine("Type your commands here:\n" + sb1.ToString());
+                    Console.Write(">:");
+                    choice = Console.ReadLine().ToString().ToLower();
+                    switch (choice)
+                    {
+                        case "add":
+                            add(num1, num2);
+                            break;
+                        case "sub":
+                            sub(num1, num2);
+                            break;
+                        case "mult":
+                            mult(num1, num2);
+                            break;
+                        case "div":
+                            div(num1, num2);
+                            break;
+                        case "exit":
+                            break;
+                        default:
+                            //default command error message 
+                            Console.WriteLine("Bad Command");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Something is wrong!");
+                }
+            } while (choice != "exit");
         }
     }
 }
