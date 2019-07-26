7d5b5ad607f8687c7988aedd7a6a7104152ee781
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-26 14:07:12 -0500

    added methods for math oprations

--- ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/Program.cs
@@ -5,6 +5,30 @@ namespace ConsoleAppCalculator
 {
     class Program
     {
+        static void add(double n1, double n2)
+        {
+            Console.WriteLine("The Result is:{0}\n", n1 + n2);
+        }
+        static void sub(double n1, double n2)
+        {
+            Console.WriteLine("The Result is:{0)\n", n1 - n2);
+        }
+        static void mult(double n1, double n2)
+        {
+            Console.WriteLine("The Result is:{0}\n", n1 * n2);
+        }
+        static void div(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                Console.WriteLine("ALERT: Division by zero is invalid!");
+
+            }
+            else
+            {
+                Console.WriteLine("The Result is:{0}\n", n1 / n2);
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("--- This is an arithmetic claculator ---");
@@ -16,6 +40,30 @@ namespace ConsoleAppCalculator
             Console.WriteLine("Type your commands here:\n" + sb1.ToString());
             Console.Write(">:");
             string choice = Console.ReadLine().ToString().ToLower();
+            Console.WriteLine("Enter two numbers:");
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            int num1 = int.Parse(n1);
+            int num2 = int.Parse(n2);
+            switch (choice)
+            {
+                case "add":
+                    add(num1, num2);
+                    break;
+                case "sub":
+                    sub(num1, num2);
+                    break;
+                case "mult":
+                    mult(num1, num2);
+                    break;
+                case "div":
+                    div(num1, num2);
+                    break;
+                default:
+                    //default command error message 
+                    Console.WriteLine("Bad Command");
+                    break;
+            }
         }
     }
 }
